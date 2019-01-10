656b07a77f22c835b5026110f19e00b536e11fd3
Lauren Tang <lauren@Laurens-MacBook-Pro-2.local>
2019-01-10 11:03:27 -0500

    Basic Activity View Layout

---
+++ app/src/main/java/com/summit/summitproject/MerchantTerminalActivity.java
@@ -0,0 +1,13 @@
+package com.summit.summitproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MerchantTerminalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merchant_terminal);
+    }
+}
