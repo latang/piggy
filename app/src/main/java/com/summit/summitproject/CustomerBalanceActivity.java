44eb9efb0a4fb5aea724ed4ea04c60fbf364e8c9
Lauren Tang <lauren@Laurens-MacBook-Pro-2.local>
2019-01-10 13:21:52 -0500

    Working nav bar on customer activity

--- app/src/main/java/com/summit/summitproject/CustomerBalanceActivity.java
+++ app/src/main/java/com/summit/summitproject/CustomerBalanceActivity.java
@@ -1,10 +1,14 @@
 package com.summit.summitproject;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class CustomerBalanceActivity extends AppCompatActivity {
 
@@ -15,7 +19,36 @@ public class CustomerBalanceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_balance);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
         mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                // Respond when the drawer's position changes
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Respond when the drawer is opened
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Respond when the drawer is closed
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                // Respond when the drawer motion state changes
+            }
+        });
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
@@ -33,5 +66,15 @@ public class CustomerBalanceActivity extends AppCompatActivity {
                         return true;
                     }
                 });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
