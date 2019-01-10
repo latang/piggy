8e6561007ff9b9d25bd45ab56c3a04f7df5a3042
theedwinyu <theedwinyu888@gmail.com>
2019-01-10 13:07:11 -0500

    Added functionality to send button on MerchantTerminal

--- app/src/main/java/com/summit/summitproject/MerchantTerminalActivity.java
+++ app/src/main/java/com/summit/summitproject/MerchantTerminalActivity.java
@@ -4,6 +4,10 @@ import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -54,10 +58,21 @@ public class MerchantTerminalActivity extends AppCompatActivity {
     }
 
     private void handleTaggedDiscover(Tag tag) {
-        String message = new NfcNdefTagReader().readMessageFromTag(tag);
+        final String message = new NfcNdefTagReader().readMessageFromTag(tag);
         if (message != null) {
-            showToast("Message: $message");
-            customerPhoneNum.setText(message);
+//            showToast("Message: $message");
+//            Log.d("debug","message: " + message);
+
+            runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    // Stuff that updates the UI
+                    customerPhoneNum.setText(message);
+                }
+            });
+
         } else {
             showToast("Message was not valid");
         }
@@ -76,8 +91,43 @@ public class MerchantTerminalActivity extends AppCompatActivity {
     }
 
     private void setUpWidgets(){
+        sendAmount.addTextChangedListener(textWatcher);
 
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputPhoneNum = customerPhoneNum.getText().toString().trim();
+                String inputSendAmount = sendAmount.getText().toString().trim();
+
+                double change = Double.parseDouble(inputSendAmount);
+
+                Log.d("debug", "change: " + change);
+
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String inputPhoneNum = customerPhoneNum.getText().toString().trim();
+            String inputSendAmount = sendAmount.getText().toString().trim();
+
+            boolean enable = inputPhoneNum.length() > 0 && inputSendAmount.length() > 0;
+
+            sendButton.setEnabled(enable);
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 
 }
