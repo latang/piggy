661c63b791dd94647c1faeaf72389112092f2686
Lauren Tang <lauren@Laurens-MacBook-Pro-2.local>
2019-01-11 00:29:26 -0500

    Added structure for customer activity

---
+++ app/src/main/java/com/summit/summitproject/prebuilt/recycler/ItemShopping.java
@@ -0,0 +1,25 @@
+package com.summit.summitproject.prebuilt.recycler;
+
+/**
+ * Created by Dytstudio.
+ */
+
+public class ItemShopping {
+    String name, pieces;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPieces(String pieces) {
+        this.pieces = pieces;
+    }
+
+    public String getPieces() {
+        return pieces;
+    }
+}
